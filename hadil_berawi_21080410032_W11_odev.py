1dceb2612a84fcc3af66bc2612eb8dcf7b545d98
Hadilberavi <97343806+Hadilberavi@users.noreply.github.com>
2022-01-09 15:33:24 +0300

    Update hadil_berawi_21080410032_W11_odev.py

--- hadil_berawi_21080410032_W11_odev.py
+++ hadil_berawi_21080410032_W11_odev.py
@@ -1,18 +1,18 @@
 from types import DynamicClassAttribute
 #Matrisi tanımlarız ve elemanları içine koyarız.
-kisilar = ['mohamad','ahmad','murat','ali']
+man = ['majd','ahmad','muru','ali']
 # liste ekrana yazdir
-print(kisilar)
+print(man)
 # kulancidan 4 isim iste
-kulancilist = input("enter the first name")
+users = input("enter the first name")
 #kulanc girdiği isim listeye ekleyiniz ve ekrana yazdir
-kisilar.append(kulancilist)
-print(kisilar)
+man.append(users)
+print(man)
 # liste uzunlugu
-print(len(kisilar)) #(len) işlevi dizinin uzunluğunu yazdırmak için kullanılır
+print(len(man)) #(len) işlevi dizinin uzunluğunu yazdırmak için kullanılır
 
 #listenin 2. elemanini ekrana yazdiriniz
-print('kisilar[1]=',kisilar[1]) #Dizideki herhangi bir öğeye erişmek için, değerini almak veya değiştirmek için, öğenin dizin numarasını kullanırız.
+print('man[1]=',man[1]) #Dizideki herhangi bir öğeye erişmek için, değerini almak veya değiştirmek için, öğenin dizin numarasını kullanırız.
 #son elemanini sil
-kisilar.pop(-1)   # Sentence(pop), bir diziyi bellekten olduğu gibi silmek veya diziden belirli öğeleri çıkarmak için kullanılır.
-print(kisilar)  # Öğeyi sildikten sonra diziyi yazdırmayı tekrarlıyoruz
+man.pop(-1)   # Sentence(pop), bir diziyi bellekten olduğu gibi silmek veya diziden belirli öğeleri çıkarmak için kullanılır.
+print(man)  # Öğeyi sildikten sonra diziyi yazdırmayı tekrarlıyoruz
